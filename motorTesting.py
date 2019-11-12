9aade9c25c908279d89af4b511db9945486a8fd1
Demo User <debian@beaglebone.localdomain>
2019-11-12 10:24:55 +0000

    updates

--- motorTesting.py
+++ motorTesting.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import RPi.GPIO as GPIO
+
+#import RPi.GPIO as GPIO
 import time
 import BlynkLib
 
@@ -7,32 +8,34 @@ BLYNK_AUTH= 'N1px31-__MUtyURliOoGUridpQrAvlc3'
 
 blynk = BlynkLib.Blynk(BLYNK_AUTH)
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(19, GPIO.OUTPUT)
-GPIO.setup(20, GPIO.OUTPUT)
-GPIO.setup(21, GPIO.OUTPUT)
-GPIO.setup(22, GPIO.OUTPUT)
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(19, GPIO.OUTPUT)
+#GPIO.setup(20, GPIO.OUTPUT)
+#GPIO.setup(21, GPIO.OUTPUT)
+#GPIO.setup(22, GPIO.OUTPUT)
 
-
-pwm.start(0)
+frequencyA=0
+frequencyB=0
+#pwm.start(0)
 
 @blynk.on("V3")
-def v3_write_handler("V3"):
-	frequencyA = args[0]
+def v3_write_handler(value):
+	frequencyA = value[0]
 #	frequencyA = 1/(args[0])    	#try this line if it is not working chart of input signals says 1/PWM
 
 @blynk.on("V4")
-def v4_write_handler("V4"):
+def v4_write_handler(value):
 	frequencyB = args[0]
-#	frequencyB = args[0]		#try this line if it is not working chart of input signals says 1/PWM
+#	frequencyB = 1/args[0]		#try this line if it is not working chart of input signals says 1/PWM
 
 while True:
 	blynk.run()
 
-	GPIO.PWM(19, frequencyA)
+#	GPIO.PWM(19, frequencyA)
 #	GPIO.PWM(20, frequencyA)
-	GPIO.PWM(21, frequencyB)
+#	GPIO.PWM(21, frequencyB)
 #	GPIO.PWM(22, frequencyB)
 #	time.sleep(10)
 	print(frequencyA)		#debug print statements
-	print(,  frequencyB)		#debug print statements
+	print(", ")			#debug print statements
+	print(frequencyB)
