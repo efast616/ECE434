0641736702fef53ee743cd02ab39682e39ebfc05
Eric Fast <Eric Fast>
2019-11-18 18:30:08 -0500

    added sleep time to slow down motors

--- motorTesting.py
+++ motorTesting.py
@@ -22,7 +22,7 @@ def write_virtual_pin_handler(pin, value):
 
 @blynk.handle_event('write V4')
 def write_virtual_pin_handler(pin, value):
-	print(value[0])
+	sleeptime = (value[0])
 
 @blynk.handle_event('write V5')			#drive the tank forward
 def write_virtual_pin_handler(pin, value):
@@ -33,28 +33,29 @@ def write_virtual_pin_handler(pin, value):
 
 @blynk.handle_event('write V6')			#drive the tank backward
 def write_virtual_pin_handler(pin, value):
-        GPIO.output(11, GPIO.LOW) 
-        GPIO.output(13, GPIO.HIGH)
+	GPIO.output(11, GPIO.LOW) 
+	GPIO.output(13, GPIO.HIGH)
 	GPIO.output(15, GPIO.LOW) 
-        GPIO.output(16, GPIO.HIGH)
+	GPIO.output(16, GPIO.HIGH)
 
 @blynk.handle_event('write V7')			#turn towards motor 2 side
 def write_virtual_pin_handler(pin, value):
-        GPIO.output(11, GPIO.HIGH) 
-        GPIO.output(13, GPIO.LOW)
+	GPIO.output(11, GPIO.HIGH) 
+	GPIO.output(13, GPIO.LOW)
 	GPIO.output(15, GPIO.LOW) 
-        GPIO.output(16, GPIO.HIGH)
+	GPIO.output(16, GPIO.HIGH)
 
 @blynk.handle_event('write V8')
 def write_virtual_pin_handler(pin, value):	#turn towards motor 1 side
-        GPIO.output(11, GPIO.LOW)
-        GPIO.output(13, GPIO.HIGH)
+	GPIO.output(11, GPIO.LOW)
+	GPIO.output(13, GPIO.HIGH)
 	GPIO.output(15, GPIO.HIGH)
-        GPIO.output(16, GPIO.LOW)
+	GPIO.output(16, GPIO.LOW)
+
 
 while True:
 	blynk.run()
-
+	time.sleep(sleeptime)
 #	GPIO.PWM(19, frequencyA)
 #	GPIO.PWM(20, frequencyA)
 #	GPIO.PWM(21, frequencyB)
